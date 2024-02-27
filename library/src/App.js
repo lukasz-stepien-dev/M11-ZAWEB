249ccff9e3d1274e91699a58f489780df574349c
Łukasz Stępień <lukaszatepien@gmail.com>
2024-02-27 15:15:57 +0100

    Recommended 27-02-24

--- library/src/App.js
+++ library/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Recommended from "./components/Recommended";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+        <Header />
+        <Recommended />
     </div>
   );
 }
---
+++ library/src/components/Recommended.jsx
@@ -0,0 +1,12 @@
+import './Recommended.css';
+
+export default function Recommended() {
+    return <section className={'recommended'}>
+        <h2>Polecamy dzieła autorów:</h2>
+        <ol>
+            <li>Jan Brzechwa</li>
+            <li>Joanna Chmielewska</li>
+            <li>Aleksander Fredro</li>
+        </ol>
+    </section>
+}
