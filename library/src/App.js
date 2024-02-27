bbaaee30e8d1dd9e3a6a0f4678669f3387943251
Łukasz Stępień <lukaszatepien@gmail.com>
2024-02-27 15:57:09 +0100

    Form 27-02-24

--- library/src/App.js
+++ library/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Recommended from "./components/Recommended";
 import AboutUs from "./components/AboutUs";
 import Form from "./components/Form";
+import Info from "./components/Info";
 
 function App() {
   return (
@@ -10,7 +11,10 @@ function App() {
         <Header />
         <Recommended />
         <AboutUs />
-        <Form />
+        <section className={'userForm'}>
+            <Form />
+            <Info />
+        </section>
     </div>
   );
 }
---
+++ library/src/components/Info.jsx
@@ -0,0 +1,7 @@
+import './Info.css'
+
+export default function Info() {
+    return <section className={'info'}>
+        <p>Czytelnik Anna Nowakowska został(a) dodany do bazy danych</p>
+    </section>
+}
