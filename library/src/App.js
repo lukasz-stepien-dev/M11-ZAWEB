eaf05301d3c97662e36d664ee3b0d9629a6db788
Łukasz Stępień <lukaszatepien@gmail.com>
2024-02-27 15:38:52 +0100

    About Us 27-02-24

--- library/src/App.js
+++ library/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Recommended from "./components/Recommended";
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Recommended />
+        <AboutUs />
     </div>
   );
 }
