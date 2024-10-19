61de8dc84c20039644eea8be6de82ba4ffd8dd21
Łukasz Stępień <lukaszatepien@gmail.com>
2024-10-19 19:34:47 +0200

    30-09-24-actors Information about actors

--- 30-09-24-actors/src/components/Content.jsx
+++ 30-09-24-actors/src/components/Content.jsx
@@ -4,9 +4,18 @@ import leslieImg from '../img/Leslie_Nielsen.jpg';
 const content = [
     {
         image: rowanImg,
-        firstName: 'Rowan Atkinson'
+        firstName: 'Rowan',
+        lastName: 'Atkinson',
+        description: 'Rowan Sebastian Atkinson (ur. 6 stycznia 1955 w Consett) – brytyjski aktor, komik, scenarzysta i producent filmowy. Najbardziej znany z ról Mr. Beana w serialu telewizyjnym i filmach fabularnych o tym samym tytule, a także z roli Edmunda Blackadder w serialu Czarna Żmija.',
+        bioName: 'Link do biografii',
+        bioUrl: 'https://en.wikipedia.org/wiki/Rowan_Atkinson',
     },
     {
         image: leslieImg,
+        firstName: 'Leslie',
+        lastName: 'Nielsen',
+        description: 'Leslie Wiliam Nielsen (ur. 11 lutego 1926 w Regina, zm. 28 listopada 2010 w Fort Lauderdale) – kanadyjski aktor filmowy i telewizyjny, komik, producent filmowy. Wystąpił w ponad 100 filmach i 150 produkcjach telewizyjnych. Najbardziej znany z ról w filmach komediowych, takich jak: Gdzie jest puma?, Policjanci z Miami, Nagi rewolwerowiec, Airplane! oraz jego kontynuacji Airplane 2: The Sequel.',
+        bioName: 'Link do biografii',
+        bioUrl: 'https://en.wikipedia.org/wiki/Leslie_Nielsen',
     }
 ]
