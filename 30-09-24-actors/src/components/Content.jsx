f14674a10d02a077ec8d6531ad86c1a17cd781f0
lukasz-stepien-dev <lukaszatepien@gmail.com>
2024-10-19 19:12:23 +0200

    30-09-24-actors init and add images

---
+++ 30-09-24-actors/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import rowanImg from '../img/Rowan_Atkinson.jpg';
+import leslieImg from '../img/Leslie_Nielsen.jpg';
+
+const content = [
+    {
+        image: rowanImg,
+        firstName: 'Rowan Atkinson'
+    },
+    {
+        image: leslieImg,
+    }
+]
